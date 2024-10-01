f4ebf6484439388a393e78b529eb3fd78c7f33b1
Jonas-Keusch <jonaskeusch4@gmail.com>
2024-10-01 12:59:32 -0400

    done

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+i =1
 
+while i <=50:
+    if i%5==0:
+        print("%d HiFive"%i)
+    elif i%2==0:
+        print("%d HiTwo"%i)
+    elif (i%3==0) or (i%7==0):
+        print("%d HiThreeOrSeven"%i)
+    i+=1
